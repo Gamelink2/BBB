a0ef1bf7de9ce2adbf19b8b9ed9fdda449fc261d
Gamelink2 <nick1klaassen@gmail.com>
2023-11-30 12:21:02 +0100

    Map page
    working map page with some started php functionality and thats it

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+var HtmlContent = require('./mapPage.html');
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-    
+          <HtmlContent/>
       </header>
     </div>
   );
---
+++ src/mapInfo.php
@@ -0,0 +1,8 @@
+<?php
+
+$areaId = $_GET['areaId'];
+
+echo '<h2>Data for Area ' . $areaId . '</h2>';
+echo '<p>' . $data . '</p>';
+
+?>
