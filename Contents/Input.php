9e98b35d1bf2aba828d0df6e0ca250d71b197b8a
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-08 11:53:00 +0100

    Update and rename Docker_Pull/Database_Interaction/Input.php to Contents/Input.php

--- Contents/Input.php
+++ Contents/Input.php
@@ -5,7 +5,7 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_NAME', 'Reserveringen');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'ROOT_PASSWORD');
 
 
 global $PDO;
