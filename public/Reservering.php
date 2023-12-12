b951f541110fdc15d1acfbafff008715865d673f
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-12 14:44:47 +0100

    Update Reservering.php
    Autocomplete on

--- public/Reservering.php
+++ public/Reservering.php
@@ -14,26 +14,26 @@ if(isset($_SESSION['ErrorMessage'])&&isset($_SESSION['ErrorMessage'])){
     <div class="bodyReservering">
     <container class="reserveringContainer">
     <h1>Reserveren voor xx/xx/xxxx t/m xx/xx/xxx</h1>
-    <form id="contactForm" method="post" action="Input.php">
-        <input class="email" type="email" id="femail" name="femail" placeholder="Email:"/> 
-        <select name="fname" id="fname" onchange="aanhefCheck(selected);">
-            <option value="" disabled selected hidden>Hoe wilt u geaddreseerd worden</option>
+        <form id="contactForm" method="post" action="Input.php">
+        <input class="email" type="email" id="femail" name="femail" placeholder="Email:" autocomplete="email" /> 
+        <select name="fname" id="fname" onchange="aanhefCheck(this.value);" autocomplete="honorific-prefix">
+            <option value="" disabled selected hidden>Hoe wilt u geaddresseerd worden</option>
             <option value="Heer">Heer</option>
             <option value="Mvr.">Mvr.</option>
             <option value="Geen">Geen</option>
             <option value="Anders:">Anders:</option>
         </select>   
-        <input type="text" id="anders" name="anders" style="display: none;" placeholder="Aanhef:"/>
-        <input type="text" id="voornaam" name="voornaam" placeholder="Vooraam:"/>
-        <input type="text" id="tussen" name="tussen" placeholder="Tussenvoegsel:"/>
-        <input type="text" id="achternaam" name="achternaam" placeholder="Achternaam:"/>
-        <input type="tel" id="telNmr" name="telNmr" placeholder="Telefoon nummer: (met landcode)"/>
-        <input class="postcode" type="text" id="postcode" name="postcode" placeholder="Postcode:"/>
-        <input class="straatnaam" type="text" id="straat" name="straat" placeholder="Straatnaam:"/>
-        <input class="huisnummer" type="text" id="huisNmr" name="huisNmr" placeholder="Huisnummer:"/>
-        <input class="toevoeging" type="text" id="huisNmr+" name="huisNmr+" placeholder="Toevoeging:"/>
-        <input type="text" id="gemeente" name="gemeente" placeholder="Gemeente:"/>
-        <input type="text" id="land" name="land" placeholder="Land:"/>
+        <input type="text" id="anders" name="anders" style="display: none;" placeholder="Aanhef:" autocomplete="honorific-suffix" />
+        <input type="text" id="voornaam" name="voornaam" placeholder="Voornaam:" autocomplete="given-name" />
+        <input type="text" id="tussen" name="tussen" placeholder="Tussenvoegsel:" autocomplete="additional-name" />
+        <input type="text" id="achternaam" name="achternaam" placeholder="Achternaam:" autocomplete="family-name" />
+        <input type="tel" id="telNmr" name="telNmr" placeholder="Telefoonnummer: (met landcode)" autocomplete="tel" />
+        <input class="postcode" type="text" id="postcode" name="postcode" placeholder="Postcode:" autocomplete="postal-code" />
+        <input class="straatnaam" type="text" id="straat" name="straat" placeholder="Straatnaam:" autocomplete="address-line1" />
+        <input class="huisnummer" type="text" id="huisNmr" name="huisNmr" placeholder="Huisnummer:" autocomplete="address-line2" />
+        <input class="toevoeging" type="text" id="huisNmr+" name="huisNmr+" placeholder="Toevoeging:" autocomplete="address-line3" />
+        <input type="text" id="gemeente" name="gemeente" placeholder="Gemeente:" autocomplete="address-level2" />
+        <input type="text" id="land" name="land" placeholder="Land:" autocomplete="country" />
         <select name="middelen" id="middelen" required>
             <option value="">Waarmee komt u kamperen</option>
             <option value="Tent">Tent</option>
