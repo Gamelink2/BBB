7f243875090a1739f78a42b6511e2b9c950f5378
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-13 17:33:42 +0100

    Update popup.php

--- dummie folder/popup.php
+++ dummie folder/popup.php
@@ -1,42 +1,7 @@
 <?php
-    include_once 'includes/dbPage.inc.php';
-?>
+include_once 'includes/dbPage.inc.php';
 
-
-
-<!DOCTYPE html>
-<html>
-<head>
-  <!--   toggle knop heeft deze shit nodig... en die form hieronder moet weg voor m om te werken... <p id="plekData"></p>
-  <button name='button' id='1' onclick="showData()">ShowData plsss</button>
-
--->
-
-<form method="post" action="" >
-        <input type="submit" name="showData" value="GO" />
-</form>
-
-<?php
-
-if (isset($_POST['showData'])) {
-    show();
-    unset($_POST['showData']);
-
-}
-
-?>
-
-<!-- dit is dan nu een stukje wat ik geprobeerd werkend te maken vanaf die link die je stuurde... werkt nu niet tho...-->
-
-</head>
-<body>
-
-<?php
- 
-
-
-
- function show($conn) {
+function show($conn) {
     $sql = "SELECT * FROM popupInhoud;";
     $result = mysqli_query($conn, $sql);
     
@@ -47,55 +12,36 @@ if (isset($_POST['showData'])) {
         $resultCheck = mysqli_num_rows($result);
         
         if ($resultCheck > 0) {
-            foreach ($row = mysqli_fetch_assoc($result)) {
+            while ($row = mysqli_fetch_assoc($result)) {
                 echo $row['PlekID'] . "<br>";            
                 echo $row['lengte'] . "<br>";
                 echo $row['breedte'] . "<br>";
             }
         } else {
-            echo "No results found.";
+            echo "Geen resultaten.";
         }
     }
 }
- 
-
-
-
-
-
 
 ?>
 
-<!-- dit onderstaande script is gwn een toggle dingetje, als je echo lengte() veranderd print ie wat je er neer zet...
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Title</title>
+</head>
+<body>
 
-<script>
-function showData()
-{
-var id=document.getElementsByName("button")[0].id;
-if(id==1)
-{
-    document.getElementById("plekData").innerHTML = echo lengte();
-    document.getElementsByName("button")[0].id=0;
-}
-else
-{
-    document.getElementById("plekData").innerHTML = "";
-    document.getElementsByName("button")[0].id=1;
-}
-}
+<form method="post" action="">
+    <input type="submit" name="showData" value="GO" />
 
-
-</script>
-
-
--->
-
-
-
-
-    
-
-
+    <?
+    if (isset($_POST['showData'])) {
+        show($conn);
+        unset($_POST['showData']);
+    }
+    ?>
+</form>
 
 
 </body>
