b46dcf3cda83f56a51949d00d844c6181321473b
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-13 09:12:14 +0100

    Update Input.php
    Just tried something, dont know if it'll work

--- public/Input.php
+++ public/Input.php
@@ -28,19 +28,60 @@ if (!empty($VoorNaam) && !empty($AchterNaam) && !empty($TelefoonNummer) && !empt
     $stmt->bindParam(':userIP', $userIP, PDO::PARAM_STR);
     $stmt->bindParam(':Username', $Username, PDO::PARAM_STR);
     if ($stmt->execute()) {
-        $Message = "reservering is toegevoegd";
-    };
+        $ErrorMessage = "reservering is toegevoegd";
+        // Send confirmation email
 
-}else {
-    $Message = "Vul alle gegevens in.";
+        $to = $Email; // note the comma
+
+        // Subject
+        $subject = 'Birthday Reminders for August';
+
+        // Message
+        $message = '
+            <html>
+            <head>
+            <title>Birthday Reminders for August</title>
+            </head>
+            <body>
+            <p>Here are the birthdays upcoming in August!</p>
+            <table>
+                <tr>
+                <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+                </tr>
+                <tr>
+                <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
+                </tr>
+                <tr>
+                <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+                </tr>
+            </table>
+            </body>
+            </html>
+            ';
+
+        // To send HTML mail, the Content-type header must be set
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+        // Additional headers
+        $headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+        $headers[] = 'From: Birthday Reminder <birthday@example.com>';
+        $headers[] = 'Cc: birthdayarchive@example.com';
+        $headers[] = 'Bcc: birthdaycheck@example.com';
+
+        // Mail it
+        mail($to, $subject, $message, implode("\r\n", $headers));
+
+
+    } else {
+        $ErrorMessage = 'Er is iets fout gegaan, probeer later opnieuw';
+    };
+} else {
+    $ErrorMessage = "Vul alle gegevens in.";
 }
 
-$_SESSION['ErrorMessage'] = $Message;
-    echo "<script> 
-            aler:'.$e.'
-            window.history.go(-1);
-          </script>";
+$_SESSION['ErrorMessage'] = $ErrorMessage;
+header($_SERVER['HTTP_REFERER']);
 
-          
 
-    echo '<script>window.close</script>;';
+die();
