74695815dde0503b71cc6b6a256ff090952d9a65
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-14 13:48:23 +0100

    Update popup.php

--- dummie folder/popup.php
+++ dummie folder/popup.php
@@ -2,26 +2,27 @@
 include_once 'includes/dbPage.inc.php';
 
 function show($conn) {
-    $sql = "SELECT * FROM popupInhoud;";
-    $result = mysqli_query($conn, $sql);
-    
-    if (!$result) {
-        // Handle query error
-        echo "Error: " . mysqli_error($conn);
-    } else {
-        $resultCheck = mysqli_num_rows($result);
-        
-        if ($resultCheck > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo $row['PlekID'] . "<br>";            
-                echo $row['lengte'] . "<br>";
-                echo $row['breedte'] . "<br>";
-            }
-        } else {
-            echo "Geen resultaten.";
-        }
+// En dan alleen zorgen dat boer bert hier als enige bij kan
+// Haal alle resultaten op uit de database
+$sql = "SELECT * FROM plekken";
+$result = $PDO->query($sql);
+
+if ($result) {
+    // Haal het aantal rijen op
+    $rowCount = $result->rowCount();
+
+    // Toon het aantal resultaten
+    // echo "Totaal aantal resultaten: " . $rowCount . "<br>"; <-- Debug, zodat je zeker weet dat er iets staat
+
+    // Toon elk resultaat aanvankelijk verborgen
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "<p class='Pop-Up' id='spot_" . $row['id'] . "' style='display: none;'>De plek is (lengte bij breedte) "
+        . $row['length'] ." x ". $row['width']  . " meter "
+        . $row["Afstand"] . ".</p>";
     }
 }
+?>
+
 
 ?>
 
