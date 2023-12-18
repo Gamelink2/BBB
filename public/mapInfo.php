1297934220a25ae5eba75cfafa5fa728421a5db4
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-18 14:37:10 +0100

    Update mapInfo.php
    F ME :cry:

--- public/mapInfo.php
+++ public/mapInfo.php
@@ -2,25 +2,31 @@
 include_once 'Connection.php';
 
 function show($conn) {
-    if(isset($_POST['value'])) {
-        $areaId = $_POST['value'];
-        // Fetch results from the database
-        $sql = "SELECT * FROM popupInhoud WHERE PlekID = $areaId;";
-        $result = mysqli_query($conn, $sql);
-        
-        if ($result) {
-            // Display each result
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<p class='Pop-Up' id='spot_1" . $row['PlekID'] . "' style='display: block;'>
-                De plek is (lengte bij breedte) "
-                    . $row['lengte'] ." x ". $row['breedte']  . " meter " . ".</p>";
-            }
+    $areaId = $_POST['value'];
+    // Haal alle resultaten op uit de database
+    $sql = "SELECT * FROM popupInhoud WHERE PlekID = $areaId;";
+    $result = mysqli_query($conn, $sql);
+    
+    if ($result) {
+        // Haal het aantal rijen op
+        $rowCount = mysqli_num_rows($result);
+
+        // Toon het aantal resultaten
+        // echo "Totaal aantal resultaten: " . $rowCount . "<br>";
+
+        // Toon elk resultaat aanvankelijk verborgen
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<p class='Pop-Up' id='spot_1" . $row['PlekID'] . " display= 'block''>
+            De plek is (lengte bij breedte) "
+                . $row['lengte'] ." x ". $row['breedte']  . " meter " . ".</p>";
         }
     }
 }
 
-// This PHP block at the top should ideally be before any HTML content
 
+
+
+//BOVENSTAANDE PHP STUKJE WERKT, HIJ GEEFT RESULTS, ALLEEN KUNNEN ZE NOG NIET WEG.
 
 ?>
 
@@ -31,32 +37,26 @@ function show($conn) {
 </head>
 <body>
 
-<form id="myForm">
-    <input value="1" type="button" onclick="loadData(1)" placeholder="plek 1"></input>
-    <input value="2" type="button" onclick="loadData(2)" placeholder="plek 2"></input>
-    <input value="3" type="button" onclick="loadData(3)" placeholder="plek 3"></input>
+<form method="post">
+        <input value="1" type="submit" placeholder="plek 1" name="value"></input>
+        <input value="2" type="submit" placeholder="plek 2" name="value"></input>
+        <input value="3" type="submit" placeholder="plek 3" name="value"></input>
 </form>
 
-<div id="content-placeholder">
+        
+
+
+
+
 <?php
-// AJAX call to submit form data without page refresh
-if(isset($_POST['value'])) {
-    show($conn);
-    exit(); // Prevent further execution
-}
-?>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<script>
-    function loadData(value) {
-        $.post('YourPHPFile.php', { value: value }, function(data) {
-            $("#content-placeholder").html(data);
-        });
+    if (isset($_POST['value'])) {
+        show($conn);
+        unset($_POST['value']);
     }
-</script>
+?>
 
-<button onclick="window.location.href='mapInfo.php';">reset knopie</button>
 
 </body>
 </html>
+
+<button onclick="window.location.href='mapInfo.php';">reset knopie</button>
