9c83d5494fe4dd9d0b0a11cedc47f1752569b8ca
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-18 14:07:56 +0100

    Update mapInfo.php

--- public/mapInfo.php
+++ public/mapInfo.php
@@ -1,24 +1,25 @@
 <?php
 include_once 'Connection.php';
 
-function show(PDO $pdo) {
+function show($conn) {
     $areaId = $_POST['value'];
-    // Prepare the query with a placeholder for the ID
-    $query = "SELECT * FROM popupInhoud WHERE PlekID = :areaId";
-    $stmt = $pdo->prepare($query);
-    $stmt->bindParam(':areaId', $areaId);
-    $stmt->execute();
+    // Haal alle resultaten op uit de database
+    $sql = "SELECT * FROM popupInhoud WHERE PlekID = $areaId;";
+    $result = mysqli_query($conn, $sql);
     
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($result) {
+        // Haal het aantal rijen op
+        $rowCount = mysqli_num_rows($result);
 
-    if (count($results) > 0) {
-        foreach ($results as $row) {
-            echo "<p class='Pop-Up' id='spot_1" . $row['PlekID'] . " display='block'>
+        // Toon het aantal resultaten
+        // echo "Totaal aantal resultaten: " . $rowCount . "<br>";
+
+        // Toon elk resultaat aanvankelijk verborgen
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<p class='Pop-Up' id='spot_1" . $row['PlekID'] . " display= 'block''>
             De plek is (lengte bij breedte) "
-            . $row['lengte'] ." x ". $row['breedte']  . " meter " . ".</p>";
+                . $row['lengte'] ." x ". $row['breedte']  . " meter " . ".</p>";
         }
-    } else {
-        echo "Geen resultaten voor deze plek.";
     }
 }
 
@@ -57,33 +58,6 @@ function show(PDO $pdo) {
 
 </body>
 </html>
-
-<?php
- //Connect with the database with the login from Connection.php
-
-if (isset($_POST['showData']) && (!empty($_POST['showData']))  ) {
-    $areaId = $_POST['PlekID'];
-    
-    // Prepare the query with a placeholder for the ID
-    $query = "SELECT * FROM Table WHERE PlekID = $areaId";
-    
-    $stmt_PlekID = $pdo->prepare($query);
-    $stmt_PlekID->execute([$areaId]);
-
-    // Fetch the result as an array
-    $result = $stmt_PlekID->fetch(PDO::FETCH_ASSOC);
-
-    // Check if there's a result for the provided ID
-    if ($result) {
-        // Generate HTML content based on the fetched data
-        echo '<h2>' . $result['PlekID'] . '</h2>';
-        echo '<p>Breedte: ' . $result['breedte'] . '</p>';
-        echo '<p>Lengte: ' . $result['lengte'] . '</p>';
-        // Add other relevant information here...
-    } else {
-        echo 'No data found for this area ID.';
-    }
-}
 
 // Je kan schijnbaar de url verandere met javascript
 // Dus als Nick de pagina laat herladen met een id van de plek, 
