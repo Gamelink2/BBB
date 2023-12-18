ad8ebf6f792f66d63427e0b23b28e8aa2c3f3e07
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-18 09:27:34 +0100

    present

--- public/Connection.php
+++ public/Connection.php
@@ -1,10 +1,10 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header("Location: index.html");
-    die();
+// if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+//     header("Location: index.html");
+//     die();
     
-}   
+// }   
 session_start();
 
 define('DB_HOST', 'localhost');
