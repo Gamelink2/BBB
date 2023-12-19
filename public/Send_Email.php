4ed2be3948680e47855ebb1f93732728cbd7b802
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-19 21:53:20 +0100

    Hush Noah, Hush

--- public/Send_Email.php
+++ public/Send_Email.php
@@ -12,13 +12,13 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';  // Your SMTP server
     $mail->SMTPAuth = true;
-    $mail->Username = 'bouwenvoorboerbert@gmail.com'; // SMTP username
-    $mail->Password = 'olilkfxcmdohbxbb'; // SMTP password
+    $mail->Username = 'PlaceHolder@gmail.com'; // SMTP username
+    $mail->Password = 'PlaceHolder'; // SMTP password
     $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587; // TCP port to connect to
 
     // Sender and recipient details
-    $mail->setFrom('bouwenvoorboerbert@gmail.com', 'Your Name'); // Sender's email address and name
+    $mail->setFrom('PlaceHolder@gmail.com', 'Your Name'); // Sender's email address and name
     $mail->addAddress($_SESSION['Email'] , $_SESSION['voornaam']); // Recipient's email address and name
 
     // Email content
@@ -31,8 +31,8 @@ try {
     $mail->send();
     echo 'Email has been sent successfully';
 } catch (Exception $e) {
-    echo "Email could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo 'Email could not be sent. Mailer Error: {$mail->ErrorInfo}';
     exit();
 }
 ?>
-<a href="index.html"><button>Terug</button></a>
+<a href='index.html'><button>Terug</button></a>
