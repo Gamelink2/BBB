47e4d6381c172ff71028c65605436020af3fb757
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-19 09:14:34 +0100

    Update Reservering.php

--- public/Reservering.php
+++ public/Reservering.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['ErrorMessage'])) {
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="./Images/BBB.ico" />
   <meta name="theme-color" content="#000000" />
@@ -17,7 +17,8 @@ if (isset($_SESSION['ErrorMessage'])) {
   />
   <title>BBB</title>
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-  <link rel="stylesheet" href="index.css"></head> 
+  <link rel="stylesheet" href="index.css">
+</head>
 <body>
     <div class="bodyReservering">
         <container class="reserveringContainer">
