125157e4c956628315d72416cc3b0a93bfc9d124
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2023-12-19 14:37:47 +0100

    Update Reservering.php

--- public/Reservering.php
+++ public/Reservering.php
@@ -25,52 +25,52 @@ session_start();
                     echo '<input class="email" type="email" id="femail" name="femail" placeholder="Email:*" autocomplete="email" required/>';
                 }
                 
-                if (isset($_SESSION["aanhef"])) { 
-                echo '<select name="fname" id="fname" onchange="aanhefCheck(this.value);" autocomplete="honorific-prefix">
-                    <option value="" disabled selected hidden>Hoe wilt u geaddresseerd worden?</option>
-                    <option value="Heer">Heer</option>
-                    <option value="Mvr.">Mvr.</option>
-                    <option value="Geen">Geen</option>
-                    <option value="Anders:">Anders:</option>
-                </select>
-                <input type="text" id="anders" name="anders" style="display: none;" placeholder="Aanhef:" autocomplete="honorific-suffix" />';
+                if (isset($_SESSION["aanhef"])) {
+                    echo '<select name="fname" id="fname" onchange="aanhefCheck(this.value);" autocomplete="honorific-prefix">
+                        <option value="" disabled selected hidden>Hoe wilt u geaddresseerd worden?</option>
+                        <option value="Heer" ' . ($_SESSION["aanhef"] == "Heer" ? 'selected' : '') . '>Heer</option>
+                        <option value="Mvr." ' . ($_SESSION["aanhef"] == "Mvr." ? 'selected' : '') . '>Mvr.</option>
+                        <option value="Geen" ' . ($_SESSION["aanhef"] == "Geen" ? 'selected' : '') . '>Geen</option>
+                        <option value="Anders:" ' . ($_SESSION["aanhef"] == "Anders:" ? 'selected' : '') . '>Anders:</option>
+                    </select>
+                    <input type="text" id="anders" name="anders" style="display: ' . ($_SESSION["aanhef"] == "Anders:" ? 'block' : 'none') . ';" placeholder="Aanhef:" autocomplete="honorific-suffix" />';
                 } else {
                     echo '<select name="fname" id="fname" onchange="aanhefCheck(this.value);" autocomplete="honorific-prefix">
-                    <option value="" disabled selected hidden>Hoe wilt u geaddresseerd worden?</option>
-                    <option value="Heer">Heer</option>
-                    <option value="Mvr.">Mvr.</option>
-                    <option value="Geen">Geen</option>
-                    <option value="Anders:">Anders:</option>
+                        <option value="" disabled selected hidden>Hoe wilt u geaddresseerd worden?</option>
+                        <option value="Heer">Heer</option>
+                        <option value="Mvr.">Mvr.</option>
+                        <option value="Geen">Geen</option>
+                        <option value="Anders:">Anders:</option>
                     </select>
                     <input type="text" id="anders" name="anders" style="display: none;" placeholder="Aanhef:" autocomplete="honorific-suffix" />';
-                    }
+                }
                     if (isset($_SESSION["voornaam"])) { 
-                    echo '<input type="text" id="voornaam" name="voornaam" placeholder="Voornaam:" value='. $_SESSION['voornaam'] .' autocomplete="given-name" required />';
+                    echo '<input type="text" id="voornaam" name="voornaam" placeholder="Voornaam:" value="'. $_SESSION['voornaam'] .'" autocomplete="given-name" required />';
                 } else {
                     echo '<input type="text" id="voornaam" name="voornaam" placeholder="Voornaam:" autocomplete="given-name" required/>';
                 }
                 if (isset($_SESSION['tussenvoegsel'])) { 
-                    echo '<input type="text" id="tussen" name="tussen" placeholder="Tussenvoegsel:" value='.$_SESSION['tussenvoegsel'].'autocomplete="additional-name" />';
+                    echo '<input type="text" id="tussen" name="tussen" placeholder="Tussenvoegsel:" value="'.$_SESSION['tussenvoegsel'].'" autocomplete="additional-name" />';
                 } else {
                     echo '<input type="text" id="tussen" name="tussen" placeholder="Tussenvoegsel:" autocomplete="additional-name" />';
                 }
                 if (isset($_SESSION['achternaam'])) { 
-                    echo '<input type="text" id="achternaam" name="achternaam" placeholder="Achternaam:" autocomplete="family-name" required/value='. $_SESSION['straatnaam'] .'>';
+                    echo '<input type="text" id="achternaam" name="achternaam" placeholder="Achternaam:" autocomplete="family-name" required value="'. $_SESSION['straatnaam'] .'"/>';
                 } else {
                     echo '<input type="text" id="achternaam" name="achternaam" placeholder="Achternaam:" autocomplete="family-name" required/>';
                 }
                 if (isset($_SESSION['nummer'])) { 
-                    echo '<input type="tel" id="telNmr" name="telNmr" placeholder="06 12345678" autocomplete="tel" inputmode="numeric" required/value='. $_SESSION['straatnaam'] .'>';
+                    echo '<input type="tel" id="telNmr" name="telNmr" placeholder="06 12345678" autocomplete="tel" inputmode="numeric" required/value="'. $_SESSION['straatnaam'] .'"/>';
                 } else {
                     echo '<input type="tel" id="telNmr" name="telNmr" placeholder="06 12345678" autocomplete="tel" inputmode="numeric" required/>';
                 }
                 if (isset($_SESSION['postcode'])) { 
-                    echo '<input class="postcode" type="text" id="postcode" name="postcode" placeholder="Postcode:" autocomplete="postal-code" value='. $_SESSION['straatnaam'] .'/>';
+                    echo '<input class="postcode" type="text" id="postcode" name="postcode" placeholder="Postcode:" autocomplete="postal-code" value="'. $_SESSION['straatnaam'] .'"/>';
                 } else {
                     echo '<input class="postcode" type="text" id="postcode" name="postcode" placeholder="Postcode:" autocomplete="postal-code" />';
                 }
                 if (isset($_SESSION['straatnaam'])) { 
-                    echo '<input class="straatnaam" type="text" id="straat" name="straat" placeholder="Straatnaam:" autocomplete="address-line1"> value='. $_SESSION['straatnaam'] .'';
+                    echo '<input class="straatnaam" type="text" id="straat" name="straat" placeholder="Straatnaam:" autocomplete="address-line1"> value="'. $_SESSION['straatnaam'] .'"/>';
                 } else {
                     echo '<input class="straatnaam" type="text" id="straat" name="straat" placeholder="Straatnaam:" autocomplete="address-line1">';
                 }
@@ -85,9 +85,9 @@ session_start();
                     echo '<input class="toevoeging" type="text" id="huisNmr+" name="huisNmr+" placeholder="Toevoeging:" autocomplete="address-line3">';
                 }
                 if (isset($_SESSION['land'])) { 
-                    echo '<input type="text" id="land" name="land" placeholder="Land:" autocomplete="country">';
+                    echo '<input type="text" id="land" name="land" placeholder="Land:" autocomplete="country" '. $_SESSION['land'] .'>';
                 } else {
-                    echo '';
+                    echo '<input type="text" id="land" name="land" placeholder="Land:" autocomplete="country">';
                 }
                 if (isset($_SESSION['middelen'])) {
                     echo '<select name="middelen" id="middelen" required>
@@ -142,6 +142,19 @@ session_start();
     </div>
     <?php
     unset($_SESSION['ErrorMessage']);
+    unset($_SESSION['email']);
+    unset($_SESSION['aanhef']);
+    unset($_SESSION['voornaam']);
+    unset($_SESSION['tussenvoegsel']);
+    unset($_SESSION['achternaam']);
+    unset($_SESSION['nummer']);
+    unset($_SESSION['postcode']);
+    unset($_SESSION['straatnaam']);
+    unset($_SESSION['huisnummer']);
+    unset($_SESSION['toevoeging']);
+    unset($_SESSION['land']);
+    unset($_SESSION['middelen']);
+    unset($_SESSION['verzoek']);
     ?>
 </body>
 </html>
