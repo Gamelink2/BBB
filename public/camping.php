6e15b7dd743dfe69d15a64769d5f37ee09e12b2d
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-15 00:53:55 +0100

    Fixed an oopsie

--- public/camping.php
+++ public/camping.php
@@ -57,7 +57,6 @@
     </div>
 
     <script>
-        <script>
             $(document).ready(function(){
                 $(".dropdown-option").click(function(e){
                     e.preventDefault(); // Prevent the default behavior of the anchor link
