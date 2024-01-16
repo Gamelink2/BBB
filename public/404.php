4873cb81445b6b8440aafff198506d670323b261
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-16 13:48:40 +0100

    css

--- public/404.php
+++ public/404.php
@@ -2,17 +2,20 @@
 <html lang="nl">
 <head>
     <?php include('navbar.php'); ?>
-    <link rel="stylesheet" href="./Css/home.css">
+    <link rel="stylesheet" href="./Css/camping.css">
 </head>
-<body class="homeBody">
-    <div class="homeBody">
-        <h1>
-            404
-        </h1>
-        <br>
-        <h2>
-            Pagina bestaat niet.
-        </h2>
+<body>
+    <div class="errorBody">
+        <div class="errors" >
+
+            <h1>
+                404
+            </h1>
+            <br>
+            <h2>
+                Pagina bestaat niet.
+            </h2>
+        </div>
     </div>
 </body>
 </html>
