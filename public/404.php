4a443a922e7d666332ba2c582263a86005779d34
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-16 11:36:54 +0100

    Error

--- public/404.php
+++ public/404.php
@@ -1,12 +1,29 @@
 <?php
 include_once("navbar.php");
 
-
 ?>
-<meta>
-<link rel="stylesheet" href="./Css/Error.css">
-</meta>
-   
-<h1>
-    404 Pagina bestaat niet.
-</h1>
+
+<style>
+    :root {
+        overflow: hidden;
+        background-image: url(./Images/Home.jpg);
+        justify-content: center;
+        text-align: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        margin: auto;
+    }
+    h1 {
+        color: whitesmoke;
+        font-size: 3rem;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        justify-self: center;
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+        margin-top: 40%;
+    }
+
+</style>
+
+   <h1>404 Pagina bestaat niet.</h1>
