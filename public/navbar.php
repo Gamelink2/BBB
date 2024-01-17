7ed2105bb31f45713d6d85e33ad57fc50fa290b9
Gamelink2 <nick1klaassen@gmail.com>
2024-01-17 22:09:00 +0100

    css

--- public/navbar.php
+++ public/navbar.php
@@ -6,7 +6,7 @@
   <link rel="icon" href="./Images/BBB.ico" />
   <meta name="description" content="BBB" />
   <title>BBB</title>
- 
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <link rel="stylesheet" href="./Css/navbar.css">
 </head>
 <body class="indexBody">
