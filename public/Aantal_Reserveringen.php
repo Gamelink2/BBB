5824a61856436818d56940c7ebcbe6946e3afd58
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-18 15:48:37 +0100

    Css?

--- public/Aantal_Reserveringen.php
+++ public/Aantal_Reserveringen.php
@@ -31,55 +31,55 @@ include_once("navbar.php");
                 </li>
             </ul>
 
-    <?php
-    $query = "SELECT * FROM persoonsgegevens";
-    $stmt = $PDO->prepare($query);
-    $stmt->execute(); // Execute the prepared statement
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    if ($results) {
-        foreach ($results as $result) {
-            ?>
-            <ul id="<?php echo $result['UserID']; ?>" class="Table-content">
-                <li>
-                    <?php echo $result['UserID']; ?>
-                </li>
-                <li>
-                    <?php echo $result['Aanhef']; ?>
-                </li>
-                <li>
-                    <?php echo $result['VoorNaam']; ?>
-                </li>
-                <li>
-                    <?php echo $result['AchterNaam']; ?>
-                </li>
-                <li>
-                    <?php echo $result['TelefoonNummer']; ?>
-                </li>
-                <li>
-                    <?php echo $result['Email']; ?>
-                </li>
-                <li>
-                    <?php echo $result['Verzoek']; ?>
-                </li>
-                <li>
-                    <?php echo $result['Aantal_Personen']; ?>
-                </li>
-                <li>
-                    <?php echo $result['Volwassenen']; ?>
-                </li>
-                <li>
-                    <?php echo $result['kinderen']; ?>
-                </li>
-            </ul>
             <?php
+            $query = "SELECT * FROM persoonsgegevens";
+            $stmt = $PDO->prepare($query);
+            $stmt->execute(); // Execute the prepared statement
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($results) {
+                foreach ($results as $result) {
+                    ?>
+                    <ul id="<?php echo $result['UserID']; ?>" class="Table-content">
+                        <li>
+                            <?php echo $result['UserID']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['Aanhef']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['VoorNaam']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['AchterNaam']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['TelefoonNummer']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['Email']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['Verzoek']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['Aantal_Personen']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['Volwassenen']; ?>
+                        </li>
+                        <li>
+                            <?php echo $result['kinderen']; ?>
+                        </li>
+                    </ul>
+                    <?php
+                }
+            } else {
+                ?>
+                <h1>Geen resultaten</h1>
+                <?php
             }
-        } else {
             ?>
-            <h1>Geen resultaten</h1>
-        <?php
-        }
-        ?>
         </div>
     </div>
 </body>
