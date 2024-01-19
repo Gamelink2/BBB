ea9737166274d64be2b7942a245764c651e26b8a
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-19 13:09:05 +0100

    Update navbar.php

--- public/navbar.php
+++ public/navbar.php
@@ -22,6 +22,9 @@
       <a href="./mapPage">Plattegrond</a>
       <a href="./Reservering">Reserveren</a>
       <a href="./mapInfo">Map Informatie</a>
+     <!-- moet eigenlijk worden afgezonderd van de rest, maar voor nu is het prima -->
+      <a href="./Aantal_Reserveringen">Reserveringen</a>
+
     </div>
   </div>
 </body>
