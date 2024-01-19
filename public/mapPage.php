048a90803bbd973acf3b3cb13efb67fb486b7ea3
Gamelink2 <nick1klaassen@gmail.com>
2024-01-19 16:41:25 +0100

    Mappage
    Hover systeem en legenda gemaakt en aan de praat gekregen

--- public/mapPage.php
+++ public/mapPage.php
@@ -4,12 +4,14 @@
     <?php include('navbar.php'); ?>
     <link rel="stylesheet" href="./Css/mapPage.css">
     <link rel="stylesheet" href="./Css/Style.css">
+    <script type="text/javascript" src="./es6-promise.js"></script>
+    <script type="text/javascript" src="./jquery.imagemapster.js"></script>
     <script>
         function showPopup(id) {
             document.getElementById('popup').style.display = 'block';
             document.getElementById('overlay').style.display = 'block';
             document.getElementById('popupContent').innerHTML = 'Clicked on area with id: ' + id;
-
+            
             fetch(`get_data.php?areaId=${areaId}`)
             .then(response => response.text())
             .then(data => {
@@ -30,25 +32,27 @@
     <container class="mapContainer">
         <container class="infoContainer">
             <div class="info">
-                <li class="staanplaatsen">Staanplaatsen</li>
-                <li>Toiletten</li>
-                <li>Zwembaden</li>
-                <li>Slipperwinkel</li>
+                <li class="staanplaatsen" onclick="groupsSelect('SP')">Staanplaatsen</li>
+                <li class="toiletten" onclick="groupsSelect('TT')">Toiletten</li>
+                <li class="zwembad" onclick="groupsSelect('ZB')">Zwembaden</li>
+                <li class="speeltuin" onclick="groupsSelect('ST')">Speeltuin</li>
+                <li class="huisBert" onclick="groupsSelect('BB')">Huis van Bert</li>
+                <li class="parkeerplekken" onclick="groupsSelect('PP')">Parkeerplekken</li>
             </div>
         </container>
         <container class="imageContainer">
-    <img src="./Images/test.png" class="test" usemap="#image-map" >
-    <map name="image-map">
-        <area id="1" alt="1" title="1" onclick="showPopup(id)" coords="200,179,44,47" shape="rect">
-        <area id="2" alt="2" title="2" onclick="showPopup(id)" coords="23,447,201,608" shape="rect">
-        <area id="3" alt="3" title="3" onclick="showPopup(id)" coords="439,449,565,569" shape="rect">
-        <area id="4" alt="4" title="4" onclick="showPopup(id)" coords="585,319,714,440" shape="rect">
-        <area id="5" alt="5" title="5" onclick="showPopup(id)" coords="438,42,563,150" shape="rect">
-        <area id="6" alt="6" title="6" onclick="showPopup(id)" coords="714,152,571,42" shape="rect">
-        <area id="7" alt="7" title="7" onclick="showPopup(id)" coords="784,49,914,154" shape="rect">
-        <area id="8" alt="8" title="8" onclick="showPopup(id)" coords="785,320,905,449" shape="rect">
-        <area id="9" alt="9" title="9" onclick="showPopup(id)" coords="919,448,1066,571" shape="rect">
-        <area id="10" alt="10" title="10" onclick="showPopup(id)" coords="930,154,1072,43" shape="rect">
+    <img src="./Images/test.png" class="test" usemap="#image-map" id="plattegrond">
+    <map name="image-map" id="image-map">
+        <area id="1" alt="1" title="1" groupKey="1, SP" onclick="showPopup(id)" href="#" coords="200,179,44,47" shape="rect">
+        <area id="2" alt="2" title="2" groupKey="2, TT" onclick="showPopup(id)" href="#" coords="23,447,201,608" shape="rect">
+        <area id="3" alt="3" title="3" groupKey="3, ZB" onclick="showPopup(id)" href="#" coords="439,449,565,569" shape="rect">
+        <area id="4" alt="4" title="4" groupKey="4, ST" onclick="showPopup(id)" href="#" coords="585,319,714,440" shape="rect">
+        <area id="5" alt="5" title="5" groupKey="5, SP" onclick="showPopup(id)" href="#" coords="438,42,563,150" shape="rect">
+        <area id="6" alt="6" title="6" groupKey="6, TT" onclick="showPopup(id)" href="#" coords="714,152,571,42" shape="rect">
+        <area id="7" alt="7" title="7" groupKey="7, ST" onclick="showPopup(id)" href="#" coords="784,49,914,154" shape="rect">
+        <area id="8" alt="8" title="8" groupKey="8, BB" onclick="showPopup(id)" href="#" coords="785,320,905,449" shape="rect">
+        <area id="9" alt="9" title="9" groupKey="9, PP" onclick="showPopup(id)" href="#" coords="919,448,1066,571" shape="rect">
+        <area id="10" alt="10" title="10" groupKey="10, PP" onclick="showPopup(id)" href="#" coords="930,154,1072,43" shape="rect">
     </map>
     </container>    
     </container>
@@ -60,6 +64,38 @@
     </div>
     </div>
 </body>
+<script>
+    function groupsSelect(group) {
+        var value = $('#plattegrond').mapster('get', group);
+
+        if (value) {
+            $('#plattegrond').mapster('set', false, group);
+        } else {
+            $('#plattegrond').mapster('set', true, group);
+        }
+    }
+
+    $(document).ready(function ()
+{
+    $('#plattegrond')
+    .mapster({
+        mapKey: 'groupKey',
+        fillColor: "d42e16",
+        stroke: true,
+        strokeColor: "ff0000",
+        strokeOpacity: 1.0,
+        strokeWidth: 2,
+        render_highlight:  {
+            fillOpacity: 0.5,
+            fillColor: "FFeedd",
+            stroke: true,
+            strokeOpacity: 1.0,
+            strokeColor: "FF0000",
+            strokeWidth: 2
+        }
+    })
+});
+</script>
 </html>
 
 
