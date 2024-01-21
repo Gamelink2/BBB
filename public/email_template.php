ad5f5bccd3278884f048e8d3c6d95d9eccefc295
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-21 12:45:32 +0100

    Update email_template.php

--- public/email_template.php
+++ public/email_template.php
@@ -2,14 +2,17 @@
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <title>E-mail sjabloon</title>
 </head>
 <body>
-    <div class="container">
         <h1>Bevestiging van uw reservering</h1>
         <p>Hallo <?php echo htmlspecialchars($VoorNaam); ?>,</p>
         <p>Uw reservering is succesvol toegevoegd!</p>
-        
+        <p>U heeft hetvolgende opgegeven:</p>
+        <li>
+            Vanaf: <?php echo htmlspecialchars($BeginDatum) ?>
+            <br>
+            tot: <?php echo htmlspecialchars($EindDatum) ?>
+        </li>
         <p>Als u vragen heeft, neem gerust <a href="">contact</a> met ons op.</p>
         <div class="footer">
             <p>Dit bericht wordt automatisch verstuurd, je kunt er niet op reageren.</p>
