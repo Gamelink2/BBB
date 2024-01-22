646b9d1c640dfe591ec65815c8ea515f84f89492
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-22 10:29:49 +0100

    Update Aantal_Reserveringen.php

--- public/Aantal_Reserveringen.php
+++ public/Aantal_Reserveringen.php
@@ -8,7 +8,8 @@ include_once("navbar.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservaties</title>
-    <link rel="stylesheet" href="Css/Aantal.css">
+    <link rel="stylesheet" href=".Css/Aantal.css">
+    <meta http-equiv="refresh" content="5">
 </head>
 <body>
     <div class="BG">
