d5e031c1f492c7836a2feed31c5f76c7744f33d7
Damianko135 <148950519+korverdamian@users.noreply.github.com>
2024-01-24 11:08:15 +0100

    Update Emailjs-Template.php

--- public/Emailjs-Template.php
+++ public/Emailjs-Template.php
@@ -4,7 +4,7 @@
 <head>
     <style>
 body {
-    background-image: url(https://groeneweide.dkorver.com/Images/Home.jpg), 0.9;
+    background-image: url(https://groeneweide.dkorver.com/Images/Home.jpg);
     font-family: Arial, sans-serif;
     margin: 0;
     padding: 0;
@@ -55,29 +55,28 @@ a:hover {
         <div class="test">
             <div class="confirm">
                 <input type="email" name="from_email" style="display: none;" value='{{from_email}}'>
-                <h1>Bedankt voor uw reservering!</h1>
-                <h2>Beste <span name="FirstName">{{FirstName}}</span> <span name="LastName">{{LastName}}</span></h2>
-                <h2>U heeft gereserveerd voor <span name="StartDate">{{StartDate}}</span> tot en met <span name="EndDate">{{EndDate}}</span></h2>
-                <h2>U heeft het volgende aangegeven:</h2>
-                <ul>
-                    <li>
-                        Aantal personen: <span name="amount">{{amount}}</span><br> waarvan: <span name="grownups">{{grownups}}</span> Volwassenen en <span name="kids">{{kids}}</span> kinderen.
-                    </li>
-                    <li>
-                        Mobiele nummer:<span name="number">{{number}}</span>
-                    </li>
-                    <li>
-                        U komt met: een <span name='arrive_with'>{{arrive_with}}</span>
-                    </li>
-                    <li>
-                        Met bericht: <span name='request'>{{request}}</span>
-                    </li>
-                </ul>
+                    <h1>Bedankt voor uw reservering!</h1>
+                    <h2>Beste <span name="FirstName">{{FirstName}}</span> <span name="LastName">{{LastName}}</span></h2>
+                    <h2>U heeft gereserveerd voor <span name="StartDate">{{StartDate}}</span> tot en met <span name="EndDate">{{EndDate}}</span></h2>
+                    <h2>U heeft het volgende aangegeven:</h2>
+                    <ul>
+                        <li>
+                            Aantal personen: <span name="amount">{{amount}}</span><br> waarvan: <span name="grownups">{{grownups}}</span> Volwassenen en <span name="kids">{{kids}}</span> kinderen.
+                        </li>
+                        <li>
+                            Mobiele nummer:<span name="number">{{number}}</span>
+                        </li>
+                        <li>
+                            U komt met: een <span name='arrive_with'>{{arrive_with}}</span>
+                        </li>
+                        <li>
+                            Met bericht: <span name='request'>{{request}}</span>
+                        </li>
+                    </ul>
 
-                <h2>Wij wensen u een fijne vakantie!</h2>
-<footer>Dit is een geautomatiseerd bericht, u kunt hier niet op reageren.</footer>
+                    <h2>Wij wensen u een fijne vakantie!</h2>
+                <footer>Dit is een geautomatiseerd bericht, u kunt hier niet op reageren.</footer>
             </div>
-
         </div>
     </div>
 </body>
